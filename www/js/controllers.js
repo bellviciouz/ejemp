b304152d4800f1299e5dbe015faca9a16d15928d
bellviciouz <bell.viciouz@gmail.com>
2015-10-15 21:22:43 +0000

    inicio

---
+++ www/js/controllers.js
@@ -0,0 +1,114 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope,$ionicPopup,$cordovaSQLite) {
+    
+    
+    
+    $scope.showAlert = function() {
+     var alertPopup = $ionicPopup.alert({
+       title: 'Agenda',
+       template: 'Datos almacenados'
+     });
+   }
+   
+   $scope.guardar = function(persona) {
+ 
+        $cordovaSQLite.execute(db, 'INSERT INTO personas (nombre,apellido,telefono,email) VALUES (?,?,?,?)', [persona.nombre,persona.apellido,persona.telefono,persona.email])
+        .then(function(result) {
+            $scope.statusMessage = "Registro guardado";
+             
+             
+             
+        var alertPopup = $ionicPopup.alert({
+       title: 'Agenda',
+       template: 'Datos almacenados'
+     });
+     
+     
+     
+    
+        }, function(error) {
+            $scope.statusMessage = "Error: " + error.message;
+        })
+         
+     
+     
+     
+     
+   }
+   
+    
+    
+    
+    
+    
+    
+    
+})
+
+.controller('ChatsCtrl', function($scope, Chats,$ionicPopup,$cordovaSQLite) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+
+
+
+
+
+
+    $scope.chats = []
+$cordovaSQLite.execute(db, 'SELECT * FROM personas ORDER BY id DESC')
+            .then(
+                function(result) {
+ 
+                    if (result.rows.length > 0) {
+ 			for(var i=0;i<result.rows.length;i++)
+			{
+			
+			$scope.chats.push({ "nombre":result.rows.item(i).nombre, 
+			    "apellido":result.rows.item(i).apellido,
+			    "telefono":result.rows.item(i).telefono,
+			    "email":result.rows.item(i).email
+			    });
+                        }
+                    }
+                },
+                function(error) {
+                    $scope.statusMessage = "Error on loading: " + error.message;
+                }
+            );
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+  */
+});
